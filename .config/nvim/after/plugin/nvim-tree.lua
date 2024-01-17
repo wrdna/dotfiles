7f1531c6ff5ae77a32ce502cf712d98709533293
andrew <andrew@whopper.none>
2024-01-17 11:15:14 -0500

    added nvim

---
+++ .config/nvim/after/plugin/nvim-tree.lua
@@ -0,0 +1,41 @@
+local nvimtree = require("nvim-tree")
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+-- empty setup using defaults
+require("nvim-tree").setup()
+
+-- OR setup with some options
+require("nvim-tree").setup({
+	sort = {
+		sorter = "case_sensitive",
+	},
+	view = {
+        width = 30,
+        side = "right",
+    },
+    renderer = {
+		group_empty = true,
+	},
+    diagnostics = {
+        enable = true,
+        show_on_dirs = true,
+        icons = {
+            hint = "",
+            info = "",
+            warning = "",
+            error = "",
+        },
+    },
+	filters = {
+		dotfiles = true,
+	},
+})
+
+vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
+vim.keymap.set("n", "<leader>r", vim.cmd.NvimTreeFocus)
---
+++ .config/nvim/lua/ordrew/remap.lua
@@ -0,0 +1,4 @@
+vim.g.mapleader = " "
+
+-- Exiting
+vim.keymap.set("n", "<leader>pv", vim.cmd.q)
