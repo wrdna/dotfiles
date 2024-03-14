1c0cca4f43a629c3c7998fc69363796f6ecf1ff1
Andrew Goldberg <goldbeaj@sunypoly.edu>
2024-03-14 10:06:45 -0400

    rebind vertical scrolling

--- .config/nvim/lua/ordrew/remap.lua
+++ .config/nvim/lua/ordrew/remap.lua
@@ -1,4 +1,8 @@
 vim.g.mapleader = " "
 
+-- Rebinding vertical scrolling
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
 -- Exiting
 vim.keymap.set("n", "<leader>pv", vim.cmd.q)
